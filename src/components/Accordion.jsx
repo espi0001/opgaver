a08aa4a14caaea293a4e92582d42e285e0bac81a
Ester <espi0001@stud.kea.dk>
2024-11-05 15:29:49 +0100

    fd

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -4,16 +4,18 @@ import { useState } from "react";
 import Item from "./Item";
 
 const Accordion = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(0);
+
   return (
     <div className="relative font-inter antialiased">
       <main className="relative min-h-screen bg-slate-50 overflow-hidden">
         <section className="w-full max-w-2xl mx-auto px-4 md:px-6 py-24">
           <h1 className="text-2xl font-bold text-slate-900 mb-4">FAQs</h1>
+          {/* Vi siger, at hvis isOpen er true, så skal vi vise Item komponenten, og så skal vi give den 3 props, som er isOpen, setIsOpen og item. */}
+          {/* vi giver item en number, så vi kan se hvilken item vi er på */}
           <Item isOpen={isOpen} setIsOpen={setIsOpen} item={1} />
           <Item isOpen={isOpen} setIsOpen={setIsOpen} item={2} />
           <Item isOpen={isOpen} setIsOpen={setIsOpen} item={3} />
-          <Item isOpen={isOpen} setIsOpen={setIsOpen} item={4} />
         </section>
       </main>
 
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,30 +1,25 @@
-"use client";
-import { useState } from "react";
-
-import { IoCloseOutline } from "react-icons/io5";
 import { FaPlus, FaMinus } from "react-icons/fa";
 
-const Item = () => {
-  const [isOpen, setIsOpen] = useState(false);
+const Item = ({ isOpen, setIsOpen, item }) => {
   return (
     <div className="border-b border-slate-200 py-4">
       <header>
         <button
           onClick={() => {
-            // Det er logical &&
-            setIsOpen(!isOpen);
+            // hvis isOpen er lig med item, så skal vi sætte isOpen til 0, ellers skal vi sætte isOpen til item
+            isOpen === item ? setIsOpen(0) : setIsOpen(item);
           }}
           className="flex items-center justify-between text-black w-full py-2 font-semibold"
         >
           <span className="">Is it sunny in DK?</span>
 
-          {/* Der er en ternary operator, som tjekker om isOpen er true eller false, og så viser den enten en plus eller minus ikon */}
-          <span>{isOpen ? <FaMinus className="text-blue-900 ml-8 self-start" /> : <FaPlus className="text-black ml-8 self-start" />}</span>
+          {/* hvis isOpen er lig med item, så skal vi vise FaMinus, ellers skal vi vise FaPlus */}
+          <span>{isOpen === item ? <FaMinus className="text-blue-900 ml-8 self-start" /> : <FaPlus className="text-black ml-8 self-start" />}</span>
         </button>
       </header>
 
       {/* Her er der en if statement, som kun viser det indhold, som er i denne if statement, hvis isOpen er true */}
-      {isOpen && (
+      {isOpen === item && (
         <section className="text-sm text-slate-600 bg-slate-100 p-4">
           <p className="text-black">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam saepe expedita tempore dicta aspernatur culpa eveniet animi cumque labore impedit aperiam, iusto consectetur voluptatibus odio quam? Mollitia veritatis ut deleniti.</p>
         </section>
