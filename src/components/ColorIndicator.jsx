2b823976857c27d50b3bb3d32235cfa4b4091d5b
Ester <espi0001@stud.kea.dk>
2024-11-05 12:19:12 +0100

    småting

--- src/components/ColorIndicator.jsx
+++ src/components/ColorIndicator.jsx
@@ -1,12 +1,11 @@
 "use client";
-import { HiDotsVertical } from "react-icons/hi";
 
 const ColorIndicator = ({ bgColor, onClick }) => {
   return (
     <div className="flex flex-col items-center justify-center gap-1">
       <button onClick={onClick} className={`w-5 h-5 ${bgColor} rounded-full border-2 border-white transform transition-transform duration-300 hover:scale-150 cursor-pointer`}></button>
       {/* <div className="w-0.5 h-6 bg-gray-300 my-1"></div> */}
-      <HiDotsVertical />
+
       {/* <div className="w-0.5 h-6 bg-gray-300 my-1"></div>
       <div className="w-0.5 h-6 bg-gray-300 my-1"></div>
       <div className="w-0.5 h-6 bg-gray-300 my-1"></div>
