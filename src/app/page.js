0e48ae75f92696af271aa5dd860b2b6680bf6fbb
Ester <espi0001@stud.kea.dk>
2024-11-08 11:45:55 +0100

    route

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default async function Home() {
   let response = await fetch("https://dummyjson.com/products");
@@ -8,6 +9,7 @@ export default async function Home() {
     <div>
       {data.products.map((product) => (
         <div key={product.id}>
+          <Image src={product.thumbnail} width={250} height={250} alt={product.title} />
           <Link href={`/detaljer/${product.id}`}>{product.title}</Link>
         </div>
       ))}
