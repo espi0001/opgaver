eea47e14513d88b2edde5552b30aa27002e19910
Ester <espi0001@stud.kea.dk>
2024-11-08 11:10:50 +0100

    link og sådan

---
+++ src/app/detaljer/[id]/page.js
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Detaler om produkter</div>;
+};
+
+export default Page;
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,16 @@
-// import ToDoApp from "@/components/ToDoApp";
+import Link from "next/link";
 
-export default function Home() {
-  return <div>HOME</div>;
+export default async function Home() {
+  let response = await fetch("https://dummyjson.com/products");
+  let data = await response.json();
+
+  return (
+    <div>
+      {data.products.map((product) => (
+        <div key={product.id}>
+          <Link href={`/detaljer/${product.id}`}>{product.title}</Link>
+        </div>
+      ))}
+    </div>
+  );
 }
